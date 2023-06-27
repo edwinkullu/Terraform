701d60be1738876b1f2c244d7338d3fd38c931cc
â€œedwinkulluâ€ <edwinkullu94@gmail.com>
2023-06-27 12:20:03 +0530

    file updated

--- Aks_Cluster/main.tf
+++ Aks_Cluster/main.tf
@@ -1,8 +1,8 @@
-resource "azurerm_kubernetes_cluster" "example" {
-  name                = "example-aks1"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  dns_prefix          = "exampleaks1"
+resource "azurerm_kubernetes_cluster" "WebappProject" {
+  name                = "WebappProject-Aks1"
+  location            = azurerm_resource_group.WebappProject.location
+  resource_group_name = azurerm_resource_group.WebappProject.name
+  dns_prefix          = "WebappProjectAks1"
 
   default_node_pool {
     name       = "default"
@@ -20,12 +20,12 @@ resource "azurerm_kubernetes_cluster" "example" {
 }
 
 output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+  value     = azurerm_kubernetes_cluster.WebappProject.kube_config.0.client_certificate
   sensitive = true
 }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.example.kube_config_raw
+  value = azurerm_kubernetes_cluster.WebappProject.kube_config_raw
 
   sensitive = true
 }
