76a53689a4b356b1bf22bfef48b2bafa063ae9cf
â€œedwinkulluâ€ <edwinkullu94@gmail.com>
2023-06-27 10:39:32 +0530

    script added

---
+++ Checkdrop.ps1
@@ -0,0 +1,13 @@
+
+Test-Path System.DefaultWorkingDirectory/_testweb-CI/drop/Webapp
+
+Write-Output "=============================================================================================================="
+
+$location = Get-Location
+
+$location
+
+Write-Output "==============================================================================================================="
+
+
+Get-ChildItem System.DefaultWorkingDirectory/_testweb-CI/drop/Webapp
