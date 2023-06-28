372ac3a9d9d89032f2419e8419133deec626ab49
â€œedwinkulluâ€ <edwinkullu94@gmail.com>
2023-06-28 15:04:38 +0530

    added image pull policy

--- Deployment/deployment.tf
+++ Deployment/deployment.tf
@@ -105,6 +105,8 @@ resource "kubernetes_deployment" "webapp" {
         container {
           image = "azureregistery02.azurecr.io/webapp:202306280617"
           name  = "webapp"
+          image_pull_policy = contains([__ProjectsToDeploy__], "webapp") == true ? "Always" : "IfNotPresent"
+
           port {
             container_port = 80
           }
