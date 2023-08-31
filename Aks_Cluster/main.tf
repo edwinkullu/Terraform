4547b8fb39f743a29136d0beed9d90dba2cfb50e
Edwin kullu <edwinkullu94@gmail.com>
2023-08-31 17:06:10 +0530

    changes

--- Aks_Cluster/main.tf
+++ Aks_Cluster/main.tf
@@ -1,5 +1,19 @@
-#This script is used for create Azure Kubernetes services cluster 
 
+# Create a resource group
+resource "azurerm_resource_group" "WebappProject" {
+  name     = "WebappProject-resources"
+  location = "centralindia"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "WebappProject" {
+  name                = "WebappProject-network"
+  resource_group_name = azurerm_resource_group.WebappProject.name
+  location            = azurerm_resource_group.WebappProject.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+#This script is used for create Azure Kubernetes services cluster 
 resource "azurerm_kubernetes_cluster" "WebappProject" {
   name                = "WebappProject-Aks1"
   location            = azurerm_resource_group.WebappProject.location
--- Aks_Cluster/provider.tf
+++ Aks_Cluster/provider.tf
@@ -19,16 +19,3 @@ provider "azurerm" {
   features {}
 }
 
-# Create a resource group
-resource "azurerm_resource_group" "WebappProject" {
-  name     = "WebappProject-resources"
-  location = "centralindia"
-}
-
-# Create a virtual network within the resource group
-resource "azurerm_virtual_network" "WebappProject" {
-  name                = "WebappProject-network"
-  resource_group_name = azurerm_resource_group.WebappProject.name
-  location            = azurerm_resource_group.WebappProject.location
-  address_space       = ["10.0.0.0/16"]
-}
