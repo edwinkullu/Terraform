2e26415166e2258f91a5fe34495289107903fa2d
Edwin kullu <edwinkullu94@gmail.com>
2023-08-31 17:01:45 +0530

    edited

--- Aks_Cluster/main.tf
+++ Aks_Cluster/main.tf
@@ -21,13 +21,3 @@ resource "azurerm_kubernetes_cluster" "WebappProject" {
   }
 }
 
-output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.WebappProject.kube_config.0.client_certificate
-  sensitive = true
-}
-
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.WebappProject.kube_config_raw
-
-  sensitive = true
-}
